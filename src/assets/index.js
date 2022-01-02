54c6ab5565a4495a6bae8af7b24846b41d634642
Khimgrg <khim-bahadur.gurung@obcc.de>
2022-01-02 13:39:05 +0100

    images added

---
+++ src/assets/index.js
@@ -0,0 +1,72 @@
+import reactjs_portfolio from './work/reactjs-portfolio.png'
+import reactjs_blog from './work/reactjs-blog.png'
+import reactjs_sbuikit from './work/reactjs-sbuikit.png'
+import reactjs_events from './work/reactjs-events.png'
+import reactjs_vbanking from './work/reactjs-vbanking.png'
+import reactjs_magazine from './work/reactjs-magazine.png'
+import reactnative_meetup from './work/reactnative-meetup.png'
+import reactnative_eticket from './work/reactnative-eticket.png'
+import reactnative_bpccs from './work/reactnative-bpccs.png'
+import reactnative_rapidinfo from './work/reactnative-rapidinfo.png'
+import xamarinform_login from './work/xamarinform-login.png'
+import xamarinform_registration from './work/xamarinform-registration.png'
+import xamarinform_eticket from './work/xamarinform-eticket.png'
+import flutter_eticket from './work/flutter-eticket.png'
+import flutter_epayment from './work/flutter-epayment.png'
+import flutter_vaccation from './work/flutter-vaccation.png'
+
+import flutter_epayment_gif from './work/flutter-epayment.gif'
+import flutter_vaccation_gif from './work/flutter-vaccation.gif'
+import reactnative_bpccs_gif from './work/reactnative-bpccs.gif'
+import reactnative_rapidinfo_gif from './work/reactnative-rapidinfo.gif'
+import reactnative_meetup_gif from './work/reactnative-meetup.gif'
+import reactnative_eticket_gif from './work/reactnative-eticket.gif'
+
+import img6 from './work/6.png'
+import img7 from './work/7.jpeg'
+import img9 from './work/9.jpeg'
+
+import android from './images/android.png'
+import ios from './images/ios.png'
+import featured_article from "./images/featured-article.jpg"
+import profile from "./images/profile.png"
+import cookery from "./images/cookery.jpg"
+import business from "./images/business.jpeg"
+import interior from "./images/interior.jpeg"
+import worldmap from "./images/worldmap.png"
+
+export { 
+    reactjs_portfolio,
+    reactjs_blog,
+    reactjs_sbuikit,
+    reactjs_events,
+    reactjs_vbanking,
+    reactjs_magazine,
+    reactnative_meetup,
+    reactnative_eticket,
+    reactnative_bpccs,
+    reactnative_rapidinfo,
+    xamarinform_login,
+    xamarinform_registration,
+    xamarinform_eticket,
+    flutter_eticket,
+    flutter_epayment,
+    flutter_vaccation,
+    flutter_epayment_gif,
+    flutter_vaccation_gif,
+    reactnative_bpccs_gif,
+    reactnative_rapidinfo_gif,
+    reactnative_meetup_gif,
+    reactnative_eticket_gif,
+    img6,
+    img7,
+    img9,
+    android,
+    ios,
+    featured_article,
+    profile,
+    cookery,
+    business,
+    interior,
+    worldmap
+}
